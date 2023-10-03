52212a1474b6f21e4a0fa446b839978618678654
babar_hashe <muhammad.babar@hashehouse.com>
2023-10-03 22:34:20 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('rich App'),
+          ),
+        ),
+        body: Center(
+          child: Text('Hello World I am Bilal'),
+        ),
+      ),
+    )
+  );
+}
